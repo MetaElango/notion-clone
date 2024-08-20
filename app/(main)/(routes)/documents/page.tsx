2758c854a1b5067b74b81daef447b8a1d0d8f85b
Elango Palanisamy <elango@Elangos-Mac-mini.local>
2024-08-20 08:40:31 +0530

    completed sidebar

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,5 @@
+const DocumentsPage = () => {
+  return <div>This is a protected documents page</div>;
+};
+
+export default DocumentsPage;
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://solid-redfish-55.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
